63e8477147cad6c4552661d98b897bb88261874a
Mr. Kool <mrkool@thekoolturegroup.com>
2026-04-14 11:24:10 -0400

    fix: update Google Search Console verification token for koolevents.app

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   description:
     "made by pros, for pros. plan kool events. live unforgettable moments.",
   verification: {
-    google: "78QVh5b6jT8Q7S_yHV9Ish4JX5iZgFWTF1g3sgV63Co",
+    google: "3ab2595fe4b3bbfd",
   },
   icons: {
     icon: [
